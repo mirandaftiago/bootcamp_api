48284ea5dfc1caca9d48138a287b1cf77119c4cb
Tiago <miranda.f.tiago@gmail.com>
2021-04-07 13:16:54 +0100

    created Routes on a separate file

--- server.js
+++ server.js
@@ -1,30 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//ROUTE FILEs
+const bootcamps = require('./routes/bootcamps');
+
 //LOAD ENV VARS
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.get('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, message: 'Show all bootcamps' });
-});
-
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, message: `Show bootcamp with ID ${req.params.id}` });
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, message: 'Create new bootcamp' });
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, message: `Update bootcamp ${req.params.id}` });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, message: `Delete bootcamp with ID ${req.params.id}` });
-});
+//MOUNT ROUTERS
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
