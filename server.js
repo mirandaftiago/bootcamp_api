c12b9410c202f88b5a54e568e8d90e48a20aedea
Tiago <miranda.f.tiago@gmail.com>
2021-04-08 10:10:44 +0100

    added DB settings, added function to handle promise rejections

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-//ROUTE FILEs
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
+
 
 //LOAD ENV VARS
 dotenv.config({
   path: './config/config.env'
 });
+
+//CONNECT TO DB
+connectDB();
+
+//ROUTE FILES
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -21,6 +27,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+//HANDLE UNHANDLED PROMISE REJECTIONS
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  
+  //CLOSE SERVER AND EXIT PROCESS
+  server.close(() => process.exit(1));
+});
