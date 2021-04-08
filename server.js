49e4328dbe654e6b3fc34a96665d92e2ea49829d
Tiago <miranda.f.tiago@gmail.com>
2021-04-08 17:31:52 +0100

    body parser for JSON data

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 
 
@@ -17,6 +18,9 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+//BODY PARSER
+app.use(express.json());
+
 // DEV LOGGING MIDDLEWARE
 if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -28,12 +32,12 @@ app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
-  PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue) 
 );
 
 //HANDLE UNHANDLED PROMISE REJECTIONS
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   
   //CLOSE SERVER AND EXIT PROCESS
   server.close(() => process.exit(1));
