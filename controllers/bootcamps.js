6639febc854080cf9b73a0c35dc6f2f316aa7a66
Tiago <miranda.f.tiago@gmail.com>
2021-04-08 17:37:18 +0100

    get all bootcamps method

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,7 +3,20 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps
 // @access    Public
-exports.getBootcamps = (req, res, next) => {
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+
+    res.status(200).json({
+      success: true,
+      data: bootcamps
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false
+    })
+  }
+
   res.status(200).json({
     success: true,
     message: 'Show all bootcamps',
