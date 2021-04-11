3fa749d1ec179b591eb28b1373a184bad5edce82
Tiago <miranda.f.tiago@gmail.com>
2021-04-11 11:02:52 +0100

    added error handler middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -39,9 +39,10 @@ exports.getBootcamp = async (req, res, next) => {
     });
 
   } catch (error) {
-    res.status(400).json({
-      success: false
-    });
+    next(error);
+    // res.status(400).json({
+    //   success: false
+    // });
   }
 }
 
