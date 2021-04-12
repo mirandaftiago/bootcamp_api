ece0b1d4dc1ae1b09c436b0cf44e8b09f055812a
Tiago <miranda.f.tiago@gmail.com>
2021-04-12 19:25:46 +0100

    created courses controller

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -11,7 +11,9 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query;
 
   //req.query copy
-  const reqQuery = { ...req.query };
+  const reqQuery = {
+    ...req.query
+  };
 
   //Fields to exclude
   const removeFields = ['select', 'sort', 'page', 'limit'];
@@ -29,13 +31,13 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   query = Bootcamp.find(JSON.parse(queryStr));
 
   //Select fields
-  if(req.query.select) {
+  if (req.query.select) {
     const fields = req.query.select.split(',').join(' ');
     query = query.select(fields);
   }
 
   //Sort
-  if(req.query.sort) {
+  if (req.query.sort) {
     const sortBy = req.query.sort.split(',').join(' ');
     query = query.sort(sortBy);
   } else {
@@ -44,7 +46,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
   //Pagination
   const page = parseInt(req.query.page, 10) || 1;
-  const limit = parseInt(req.query.limit, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
   const startIndex = (page - 1) * limit;
   const endIndex = page * limit;
   const total = await Bootcamp.countDocuments();
@@ -57,15 +59,15 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   //Pagination result
   const pagination = {};
 
-  if(endIndex < total) {
+  if (endIndex < total) {
     pagination.next = {
       page: page + 1,
       limit
     }
   }
 
-  if(startIndex > 0) {
-    pagination.prev = { 
+  if (startIndex > 0) {
+    pagination.prev = {
       page: page - 1,
       limit
     }
@@ -100,12 +102,12 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @access    Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
 
-    const bootcamp = await Bootcamp.create(req.body);
+  const bootcamp = await Bootcamp.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: bootcamp
-    });
+  res.status(201).json({
+    success: true,
+    data: bootcamp
+  });
 });
 
 // @desc      Update bootcamp
@@ -113,19 +115,19 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 // @access    Public
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 
-    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    if (!bootcamp) {
-      return next(new ErrorResponse(`Bootcamp ${req.params.id} not found`, 404));
-    }
+  if (!bootcamp) {
+    return next(new ErrorResponse(`Bootcamp ${req.params.id} not found`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: bootcamp
-    });
+  res.status(200).json({
+    success: true,
+    data: bootcamp
+  });
 });
 
 // @desc      Delete single bootcamp
@@ -133,16 +135,16 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @access    Public
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
-    if (!bootcamp) {
-      return next(new ErrorResponse(`Bootcamp ${req.params.id} not found`, 404));
-    }
+  if (!bootcamp) {
+    return next(new ErrorResponse(`Bootcamp ${req.params.id} not found`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: {}
-    });
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
 });
 
 // @desc      Get bootcamps within radius
@@ -150,7 +152,10 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 // @access    Private
 exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
 
-  const { zipcode, distance } = req.params;
+  const {
+    zipcode,
+    distance
+  } = req.params;
 
   //Get Latitude and Longitude from geocoder
   const loc = await geocoder.geocode(zipcode);
@@ -164,7 +169,13 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
   const radius = distance / earthRadius;
 
   const bootcamps = await Bootcamp.find({
-    location: { $geoWithin: { $centerSphere: [ [ lng, lat], radius ]}}
+    location: {
+      $geoWithin: {
+        $centerSphere: [
+          [lng, lat], radius
+        ]
+      }
+    }
   });
 
   res.status(200).json({
