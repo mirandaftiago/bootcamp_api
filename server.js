3328e1eeab1a1aed07cf27fecee4a9ac51309acd
Tiago <miranda.f.tiago@gmail.com>
2021-04-14 19:27:05 +0100

    added auth routes

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ connectDB();
 //ROUTE FILES
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 const app = express();
 
@@ -39,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //MOUNT ROUTERS
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 app.use(errorHandler);
 
