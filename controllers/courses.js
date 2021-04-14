7998e6296cc7efab6a8a0fe1d145849a77ada10e
Tiago <miranda.f.tiago@gmail.com>
2021-04-14 10:02:32 +0100

    Delete course

--- controllers/courses.js
+++ controllers/courses.js
@@ -90,9 +90,29 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
     new: true,
     runValidators: true
   });
-  
+
   res.status(200).json({
     success: true,
     data: course
+  });
+});
+
+// @desc      Delete course
+// @route     DELETE /api/v1/courses/:id
+// @access    Private
+
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(new ErrorResponse(`No course with the id ${req.params.id}`), 404);
+  }
+
+  await course.remove();
+  
+  res.status(200).json({
+    success: true,
+    data: {}
   });
 });
--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const {
-  getCourses, getCourse, addCourse, updateCourse
+  getCourses, getCourse, addCourse, updateCourse, deleteCourse
 } = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses).post(addCourse);
-router.route('/:id').get(getCourse).put(updateCourse);
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
