c160bd87d8e8bb222c85086f0d49ddfdfb186db6
Tiago <miranda.f.tiago@gmail.com>
2021-04-14 09:35:12 +0100

    Get single course function

--- controllers/courses.js
+++ controllers/courses.js
@@ -10,8 +10,10 @@ const Course = require('../models/Course');
 exports.getCourses = asyncHandler(async (req, res, next) => {
   let query;
 
-  if(req.params.bootcampId) {
-    query = Course.find({ bootcamp: req.params.bootcampId });
+  if (req.params.bootcampId) {
+    query = Course.find({
+      bootcamp: req.params.bootcampId
+    });
   } else {
     query = Course.find().populate({
       path: 'bootcamp',
@@ -25,5 +27,26 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     success: true,
     count: courses.length,
     data: courses
+  });
+});
+
+// @desc      Get single course
+// @route     GET /api/v1/courses/:id
+// @access    Public
+
+exports.getCourse = asyncHandler(async (req, res, next) => {
+
+  const course = await (await Course.findById(req.params.id)).populate({
+    path: 'bootcamp',
+    select: 'name description'
+  });
+
+  if (!course) {
+    return next(new ErrorResponse(`No course with the id ${req.params.id}`), 404);
+  }
+
+  res.status(200).json({
+    success: true,
+    data: course
   });
 });
