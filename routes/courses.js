74fb30f33fa7580279de418d31830b29dd1f95b5
Tiago <miranda.f.tiago@gmail.com>
2021-04-14 09:58:13 +0100

    Update course

--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const {
-  getCourses, getCourse, addCourse
+  getCourses, getCourse, addCourse, updateCourse
 } = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses).post(addCourse);
-router.route('/:id').get(getCourse);
+router.route('/:id').get(getCourse).put(updateCourse);
 
 module.exports = router;
