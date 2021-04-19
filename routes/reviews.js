cf0562c642930c287b48cf0b010cfc80a768e9be
Tiago <miranda.f.tiago@gmail.com>
2021-04-19 14:26:23 +0100

    get single review

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {
-  getReviews
+  getReviews,
+  getReview
 } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
@@ -10,7 +11,10 @@ const router = express.Router({
 });
 
 const advancedResults = require('../middleware/advancedResults');
-const { protect, authorize } = require('../middleware/auth');
+const {
+  protect,
+  authorize
+} = require('../middleware/auth');
 
 router.route('/')
   .get(advancedResults(Review, {
@@ -19,5 +23,7 @@ router.route('/')
     }),
     getReviews
   );
+
+router.route('/:id').get(getReview);
 
 module.exports = router;
