45fb922c58961eb540e75967b7d70c5c1fa8ac52
Tiago <miranda.f.tiago@gmail.com>
2021-04-19 09:31:22 +0100

    Forgot password - sendEmail

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
-const Bootcamp = require('../models/Bootcamp');
+const sendEmail = require('../utils/sendEmail');
 const User = require('../models/User');
 
 // @desc      Register user
@@ -77,7 +77,9 @@ exports.getMe = asyncHandler(async (req, res, next) => {
 // @route     POST /api/v1/auth/forgotpassword
 // @access    Public
 exports.forgotPassword = asyncHandler(async (req, res, next) => {
-  const user = await User.findOne({ email: req.body.email });
+  const user = await User.findOne({
+    email: req.body.email
+  });
 
   if (!user) {
     return next(new ErrorResponse('No user with that email', 404));
@@ -86,7 +88,37 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   //Get reset token
   const resetToken = user.getResetPasswordToken();
 
-  await user.save({ validateBeforeSave: false });
+  await user.save({
+    validateBeforeSave: false
+  });
+
+  //Create reset URL
+  const resetURL = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (or someone else) has request the reset of a password. Please make a PUT request to: \n\n ${resetURL}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message
+    });
+
+    res.status(200).json({
+      success: true,
+      data: 'Email sent'
+    });
+  } catch (error) {
+    console.log(error);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({
+      validateBeforeSave: false
+    });
+
+    return next(new ErrorResponse('Email could not be sent', 500));
+  }
 
   res.status(200).json({
     success: true,
